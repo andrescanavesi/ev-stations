47b21d4d753b532197a13f1f7c1a6b968839a1ee
Andres Canavesi <andres.canavesi@gmail.com>
2023-11-20 20:33:40 -0300

    map improvements

--- utils.js
+++ utils.js
@@ -2,22 +2,23 @@ const STATIONS_DATA_SOURCE = 'https://cdn.jsdelivr.net/gh/andrescanavesi/ev-stat
 const FAST_STATION_POWER = 45;
 const CENTER_URUGUAY = { lat: -32.951477, lng: -56.114227 };
 const DEFAULT_MAP_ZOOM = 7;
+
 const fetchStations = async () =>{
     const response = await fetch(STATIONS_DATA_SOURCE);
     if (!response.ok) {
         throw new Error(`error fetching stations: ${response.status} - ${response.statusText}`);
     }
     let stations = await response.json();
-    console.info('all stations: ', stations.length)
+    //console.info('all stations: ', stations.length)
     return filterStations(stations);
 }
 
 const reload =  () =>{
-    console.info('reload')
+    //console.info('reload')
     fetchStations().then((stations)=> {
         loadStationsTable(stations);
         loadMap(stations);
-        console.info('reloaded')
+        //console.info('reloaded')
     }).catch((err)=> console.error(err))
 }
 
@@ -39,18 +40,18 @@ const filterStations = (stationsArray) => {
 
     // filter by power
     const power = getSelectedPower();
-    console.info('power', power);
+    //console.info('power', power);
     stations = stations.filter((station) => station.maxPower >= power);
 
     // filter by connector type
     const connectorName = getSelectedConnector();
-    console.info('connector', connectorName);
+    //console.info('connector', connectorName);
     if(connectorName !== 'all'){
         stations = stations.filter((station) => station.connectorStatusAcc.some((connector) => connector.type === connectorName && connector.power >= power));
     }
 
     //console.table(stations);
-    console.info('public stations loaded:', stations.length);
+    //console.info('public stations loaded:', stations.length);
     const countStationsDiv =  document.getElementById('countStations');
     if(countStationsDiv) countStationsDiv.innerHTML = stations.length;
     return sortStationsByDepartment(stations);
@@ -67,9 +68,39 @@ const getSelectedRadio = (radioName, defaultValue) => {
     const radios = document.getElementsByName(radioName);
     if(!radios) return defaultValue;
     for (let i = 0; i < radios.length; i++) {
-        if(radios[i].checked) return radios[i].value;
+        if(radios[i].checked){
+            saveSelectedRadio(radioName, i);
+            return radios[i].value;
+        }
     }
 }
+
+const saveSelectedRadio = (radioName, index) => {
+    const key = `${radioName}SelectedIndex`;
+    //console.info(`saveSelectedRadio ${key} index: ${index}`);
+    localStorage.setItem(key, index);
+}
+
+const loadSelectedRadio = (radioName) => {
+    const key = `${radioName}SelectedIndex`;
+    let index = localStorage.getItem(key);
+    //console.info(` 1 loadSelectedRadio key: ${key} index: ${index}`);
+    if(!index) index = 0;
+    else index = Number(index);
+    const radios = document.getElementsByName(radioName);
+    if(!radios) return;
+    if(!radios[index]) return;
+    //console.info(` 2 loadSelectedRadio key: ${key} index: ${index}`);
+    radios[index].checked = true;
+}
+
+const loadSelectedPower = () =>{
+    loadSelectedRadio('powerRadio')
+}
+const loadSelectedConnector = () =>{
+    loadSelectedRadio('connectorRadio')
+}
+
 
 const getSelectedPower = () => {
     return getSelectedRadio('powerRadio', 0);
@@ -111,7 +142,7 @@ const loadStationsTable = (stations)=>{
     if(!table) return;
 
     const rowCount = table.rows.length;
-    console.info('row count', rowCount)
+    //console.info('row count', rowCount)
     if(rowCount > 1){
         for (let i = 1; i < rowCount; i++) {
             table.deleteRow(1);
@@ -190,10 +221,56 @@ const loadMap = (stations)=>{
     if(!mapDiv) return;
 
     const map = new google.maps.Map(mapDiv, {
-        zoom: DEFAULT_MAP_ZOOM,
-        center: CENTER_URUGUAY,
+        zoom: getMapZoom(),
+        center: getMapCenter(),
     });
     for (const station of stations) {
         createMapMarker(station, map);
     }
+
+    map.addListener("zoom_changed", () => {
+        //console.info('zoom changed to '+map.getZoom());
+        setMapZoom(map.getZoom())
+
+    });
+
+    map.addListener("center_changed", () => {
+        //console.info('center changed to '+map.getCenter());
+        //console.info(JSON.stringify(map.getCenter()));
+        getMapCenter();
+        setMapCenter(map.getCenter());
+    });
+
+}
+
+const getMapZoom = () => {
+    let mapZoom = localStorage.getItem("mapZoom");
+    if(!mapZoom) mapZoom = DEFAULT_MAP_ZOOM;
+    //console.info('get mapZoom',mapZoom);
+    return Number(mapZoom);
+}
+
+const setMapZoom = (mapZoom) => {
+    //console.info('set mapZoom',mapZoom);
+    localStorage.setItem("mapZoom", mapZoom);
+
+}
+
+const getMapCenter = () => {
+    let mapCenterLat = localStorage.getItem("mapCenterLat");
+    let mapCenterLng = localStorage.getItem("mapCenterLng");
+    let mapCenter;
+    if(!mapCenterLat || !mapCenterLng){
+        mapCenter = CENTER_URUGUAY;
+    } else{
+        mapCenter=  {lat: parseFloat(mapCenterLat), lng: parseFloat(mapCenterLng)};
+    }
+    return mapCenter;
+}
+
+const setMapCenter = (mapCenter) => {
+    const string = JSON.stringify(mapCenter);
+    const json = JSON.parse(string);
+    localStorage.setItem("mapCenterLat", json.lat);
+    localStorage.setItem("mapCenterLng", json.lng);
 }
