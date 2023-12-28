71dc6b2649925339002a6c4a710132f5331709e1
Andres Canavesi <andres.canavesi@gmail.com>
2023-12-28 11:29:18 -0300

    Add files via upload

--- comparator.js
+++ comparator.js
@@ -1,8 +1,10 @@
-const stationsOld = require('./stations/stations-2023-11-08.json');
-const stationsNew = require('./stations/stations-2023-11-09.json');
+const stationsOldest = require('./stations/stations-2023-11-08.json');
+const stationsOld = require('./stations/stations-2023-12-24.json');
+const stationsNew = require('./stations/stations-2023-12-28.json');
 
 // it detects the new stations added
 const newOnes = [];
+let newOnesFriendly = [];
 
 for (const stationNew of stationsNew) {
     let found = false;
@@ -10,8 +12,40 @@ for (const stationNew of stationsNew) {
         if(stationOld.lat == stationNew.lat && stationOld.lng == stationNew.lng) found = true;
 
     }
-    if(!found) newOnes.push(stationNew);
+    if(!found){
+        const elem = {
+            nombre: stationNew.name,
+            departamento: stationNew.department,
+            ciudad: stationNew.city,
+            direccion: stationNew.address,
+        };
+        let connectorText = '';
+        for (const connector of stationNew.connectorStatusAcc) {
+            const conn = `${connector.type}-${connector.power}kw`;
+            if(!elem[conn]) elem[conn] = connector.count;
+        }
+        newOnesFriendly.push(elem);
+        newOnes.push(stationNew);
+    }
 }
 
-console.table(newOnes)
+const removed = [];
+for (const stationOld of stationsOldest) {
+    let found = false;
+    for (const stationNew of stationsNew) {
+        if(stationOld.lat == stationNew.lat && stationOld.lng == stationNew.lng) found = true;
+    }
+    if(!found) removed.push(stationOld);
+}
+
+
+
+console.info('new ones');
+console.table(newOnes);
+console.table(newOnesFriendly);
+//console.log(JSON.stringify(newOnes, null, 2))
+
+console.info('removed');
+console.table(removed);
+
 
