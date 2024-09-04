a846a64676d681370ff4daf5cd9be26072945c51
Andres Canavesi <andres.canavesi@gmail.com>
2024-09-04 10:49:20 -0300

    update stations

--- comparator.js
+++ comparator.js
@@ -1,6 +1,6 @@
 const stationsOldest = require('./stations/stations-2023-11-08.json');
-const stationsOld = require('./stations/stations-2024-04-16.json');
-const stationsNew = require('./stations/stations-2024-05-21.json');
+const stationsOld = require('./stations/stations-2024-05-21.json');
+const stationsNew = require('./stations/stations-2024-09-04.json');
 
 // it detects the new stations added
 const newOnes = [];
